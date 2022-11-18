897fed7345ded534f27b6ef694950d1865c12c49
ahmet menevse <ahmetmenevse26@gmail.com>
2022-11-18 13:43:32 +0800

    lists completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  resources :movies
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get 'lists', to: 'lists#index'
+  # post 'lists', to: 'lists#create'
+  # get 'lists/new', to: 'lists#new'
+  # get 'lists/:id', to: 'lists#show'
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
